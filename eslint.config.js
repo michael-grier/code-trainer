b19bb8271a8ec7a5197713ecf7925da7b190d611
Michael Grier <michael.c.grier@gmail.com>
2026-07-06 11:23:00 -0600

    chore: add convex generated files

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ['dist', 'node_modules', 'convex/_generated'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
